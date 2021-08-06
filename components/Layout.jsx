16469cdd28591fcd16b2fe814fc8ffa509932393
iCode-Cat <devranboyac8@gmail.com>
2021-08-06 13:58:11 +0300

    img tag deleted

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,10 +10,6 @@ const layout = (props) => {
       <header className={style.header}>
         <Link href='/'>
           <a>
-            <img
-              src='/api?key=020c23b95a0044fc9a1d59fbf897a365&size=small&tid=234888'
-              alt='none'
-            />
             <Image width='128px' src={Logo} alt='cat-wiki-logo' />
           </a>
         </Link>
